66be6735796a3c4386116adab97530346922cc99
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-07-06 17:58:43 +0200

    Funciones de /reacciones movidas al propio archivo del comando
    Como eran dos métodos pequeños no era necesario tenerlos separados en diferentes archivos.

--- src/commands/chatInput/reacciones.ts
+++ src/commands/chatInput/reacciones.ts
@@ -1,8 +1,8 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import { MClient } from "../../helpers/MClient";
-import { TipoComandos, ComandoChatInput } from "../../types";
+import { TipoComandos, ComandoChatInput, Reaccion } from "../../types";
 import Reacciones from "../../data/comandos/reacciones.data";
-import { generarEmbed } from "../../helpers/comandos/Reacciones";
+import { Colores } from "../../data/colores.data";
 
 const data = new SlashCommandBuilder()
     .setName("reacciones")
@@ -29,6 +29,32 @@ const exp: ComandoChatInput = {
             embeds: [embed],
         });
     },
+};
+
+/**
+ * Genera un embed con una frase de descripción, color de embed por defecto y un gif como imagen dependiendo de la reacción pasada como parámetro.
+ *
+ * En la frase reemplaza `{{usuario}}` por la mención del usuario con el id pasado como parámetro.
+ *
+ * @param {Reaccion} reaccion Reacción de la que sacar los valores
+ * @param {string} idUsuario Usuario por el que se reemplazará {{usuario}} en la frase
+ * @return {EmbedBuilder}  EmbedBuilder preparado para ser enviado
+ */
+const generarEmbed = (reaccion: Reaccion, idUsuario: string): EmbedBuilder => {
+    return new EmbedBuilder()
+        .setColor(Colores.EMBED_BASE)
+        .setImage(randomGif(reaccion.gifs))
+        .setDescription(reaccion.frase.replace("{{usuario}}", `<@${idUsuario}>`));
+};
+
+/**
+ * Devuelve un gif aleatorio de un array[] de URLs de gifs.
+ *
+ * @param {string[]} gifs Array de URLs de gifs
+ * @return {string}  URL del gif
+ */
+const randomGif = (gifs: string[]): string => {
+    return gifs[Math.floor(Math.random() * (gifs.length - 1))];
 };
 
 module.exports = exp;
