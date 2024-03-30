d2602b74dacd9f3652a2142c6e1a1ad3063f51a1
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2024-03-30 16:11:56 +0100

    Volta atrás

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -64,7 +64,7 @@ const sofiDropController = async (mcli: MClient, message: Message): Promise<void
         const lineas = message.content.split("\n");
         for (const linea of lineas) {
             let partes = linea.split("•");
-            series.push(partes[partes.length - 1].trim());
+            series.push(partes[partes.length - 1].trim().replace(/\*/g, ""));
         }
     }
 
